f0cfd7c97483ae69d2cdaa0a35099eba59f35361
nurahsally <133184778+nurahsally@users.noreply.github.com>
2023-05-14 19:29:24 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -16,11 +16,7 @@ try{
 	
 	$DBcon = new PDO("mysql:host=$DBhost;dbname=$DBname",$DBuser,$DBpass);
 	$DBcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	
-}catch(PDOException $ex){
-	
-	die($ex->getMessage());
-}
+
 
 // Clean file names
 function clean($string){
